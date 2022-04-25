0cf6524d86f734b9e61a916e7fd1af28fbc800e4
JSyndicate <jevwilliamson@gmail.com>
2022-04-25 00:20:10 -0500

    your message here

---
+++ src/views/LogOutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Login/>
+  </div>
+</template>
+<script>
+import Logout from "../components/Logout.vue";
+export default {
+    data() {
+        return {
+            message: "Logout!"
+        };
+    },
+    components: { Logout }
+};
+</script>
+
+<style>
+</style>
