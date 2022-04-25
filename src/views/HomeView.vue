309cb143a1d35894b8766894a11238c434a17cc6
RanielJohnson <91336786+RanielJohnson@users.noreply.github.com>
2022-04-25 00:15:16 -0500

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,50 +1,58 @@
-<script>
-import Register from '@/components/Register.vue';
-
-</script>
-
 <template>
-    <div class="container">
-      <div class="text-center">
-        <div id="text">
-          <h1>Buy and Sell <br> Cars Online</h1>
-          <p>United Auto Sales provides the fastest easitest friendly way to buy or sell cars online. Find a Great Price on the Vehicle on the Vehicle You want. </p>
-        <button class="btn btn-primary mb-2" id="register" name="register">Register</button>
-        <button class="btn btn-primary mb-2" id="login" name="login">Login</button>
-        <img alt="Vue logo" class="logo" src="uploads/Carimg.png" width="125" height="125" />
+      <div class="container">
+        <div class="row d-flex align-items-center">
+
+          <div class="col text-center">
+            <div class="container col-md-6 col-lg-6 col-xl-9 col-xxl-8">
+
+              <h1>{{ message }}</h1>
+              <p class="text-muted">
+                United Auto Sales provides the  fastest, easiest and most user friendly way
+                to buy or sell cars online. Find a great price on the vehicle you want.
+              </p>
+              <div class="button-group d-flex">
+                <div class="pe-3 page-nav">
+
+                  <a href="#" class="btn btn-primary" >Register</a>
+                </div>
+                
+                <div class="page-nav">
+                  <a href="#" class="btn btn-success">Login</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div class="col">
+            <img src="@/assets/photo-landingPage.jpeg" alt=""  id="landingPageImage" class="d-inline-block">
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            message: "Buy and Sell Cars Online"
+        };
+    },
+}
+</script>
+
+
 <style>
-/* Add any component specific styles here */
-h1{
-  margin-right: 150px;
-  float:left;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-p{
-  margin-right: 150px;
-  padding: 50px;
-  float:left;
-}
- button{
-   float: left;
- }
+#landingPageImage{
+  width: 40rem;
+  height: 60rem;
 
-img {
-  height: 25%;
-  width: 25%;
-  padding-bottom: 50%;
-  float: right;
 }
-.text{
-  display: flex;
-  align-items: center;
-  justify-content: center;
 
+a{
+  width: 100%;
+}
+
+.page-nav{
+  width: 10rem;
 }
 </style>
