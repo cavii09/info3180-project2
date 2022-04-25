23a11afbb9a823808792082396650643fc499d8c
JSyndicate <jevwilliamson@gmail.com>
2022-04-24 22:16:19 -0500

    your message here

---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<template>
+    <Login/>
+</template>
+<script>
+import Login from "../components/Login.vue";
+export default {
+    data() {
+        return {
+            message: "Login!"
+        };
+    },
+    components: { Login }
+};
+</script>
