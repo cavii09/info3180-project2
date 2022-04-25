5b8f4cb6cf714892991d0b6030037bbb6cfd1ef6
RanielJohnson <91336786+RanielJohnson@users.noreply.github.com>
2022-04-24 23:06:13 -0500

    Create ExploreView.vue

---
+++ src/views/ExploreView.vue
@@ -0,0 +1,15 @@
+<script>
+import Explore from "../components/Explore.vue";
+export default {
+    data() {
+      return[]; 
+    },
+    components: {Explore}        
+};
+</script>
+
+<template>
+<Explore/>   
+</template>
+
+<style> </style>
